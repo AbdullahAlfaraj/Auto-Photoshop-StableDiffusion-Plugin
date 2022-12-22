11a469c064f9300e996bdaccf5f877e233282f4b
Kyxsune <Kyxsune@users.noreply.github.com>
2022-12-22 11:58:45 -0500

    Create start_server.sh

---
+++ start_server.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Check if the desired environment exists
+if [ ! -d "server_env" ]; then
+  # Create the environment if it doesn't exist
+  python -m venv server_env
+  source ./server_env/bin/activate
+  python -m pip install -r requirements.txt
+else
+  source ./server_env/bin/activate
+fi
+
+cd ./server/python_server
+python -m uvicorn serverMain:app --reload
