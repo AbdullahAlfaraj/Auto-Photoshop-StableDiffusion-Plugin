7a90467941ce1bedd165f62ac7649da4620f60d2
Abdullah Alfaraj <abdullah.a.f2014@gmail.com>
2023-01-12 22:06:48 +0300

    tips to be injected into their corresponding html elements

---
+++ utility/tips.js
@@ -0,0 +1,12 @@
+//tips that will display when you hover over a html element
+const tips_json = {
+    txt2img: "use this mode to generate images from text only",
+    img2img: "use this mode to generate variation of an image",
+    inpaint: "use this mode to generate variation of a small area of an image, while keeping the rest of the image intact",
+    outpaint: "use this mode to (1) fill any missing area of an image,(2) expand an image",
+    generate: "create"
+
+
+}
+
+module.exports = {tips_json}
