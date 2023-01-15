0be5fd3b5d47ef83784e2f2b91be113ff4df279a
Guillaume GUARD <guillaumeguard@gmail.com>
2023-01-15 18:36:11 +0100

    added inpainting mask strengh support

--- sdapi.js
+++ sdapi.js
@@ -1,5 +1,5 @@
 //javascript plugin can't read images from local directory so we send a request to local server to read the image file and send it back to plugin as image string base64
-async function getInitImage (init_image_name) {
+async function getInitImage(init_image_name) {
   console.log('getInitImage(): get Init Image from the server :')
   const payload = {
     init_image_name: init_image_name
@@ -32,7 +32,7 @@ async function getInitImage (init_image_name) {
   // console.dir(img_blob)
 }
 
-async function requestTxt2Img (payload) {
+async function requestTxt2Img(payload) {
   try {
     console.log('requestTxt2Img(): about to send a fetch request')
 
@@ -62,9 +62,9 @@ async function requestTxt2Img (payload) {
 
 
 
-async function requestImg2Img (payload) {
+async function requestImg2Img(payload) {
   console.log('requestImg2Img(): about to send a fetch request')
-  try{
+  try {
 
     const full_url = 'http://127.0.0.1:8000/img2img/'
     console.log(full_url)
@@ -73,25 +73,25 @@ async function requestImg2Img (payload) {
       method: 'POST',
       headers: {
         Accept: 'application/json',
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify(payload)
-    // "body": payload
-  })
-  
-  let json = await request.json()
-  console.log('requestImg2Img json:')
-  console.dir(json)
-  
-  return json
-}
-catch(e){
-  console.warn(e)
-return {}
-}
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(payload)
+      // "body": payload
+    })
+
+    let json = await request.json()
+    console.log('requestImg2Img json:')
+    console.dir(json)
+
+    return json
+  }
+  catch (e) {
+    console.warn(e)
+    return {}
+  }
 }
 
-async function requestProgress () {
+async function requestProgress() {
   console.log('requestProgress: ')
 
   const full_url =
@@ -104,24 +104,24 @@ async function requestProgress () {
   return json
 }
 
-async function requestGetModels () {
+async function requestGetModels() {
   console.log('requestGetModels: ')
   let json = []
-  try{
+  try {
 
     const full_url = 'http://127.0.0.1:8000/sdapi/v1/sd-models'
     let request = await fetch(full_url)
     json = await request.json()
     console.log('models json:')
     console.dir(json)
-    
-  }catch(e){
-    console.warn(`issues requesting from ${full_url}`,e)
+
+  } catch (e) {
+    console.warn(`issues requesting from ${full_url}`, e)
   }
-    return json
+  return json
 }
 
-async function requestGetSamplers () {
+async function requestGetSamplers() {
   console.log('requestGetSamplers: ')
 
   const full_url = 'http://127.0.0.1:8000/sdapi/v1/samplers'
@@ -133,7 +133,7 @@ async function requestGetSamplers () {
   return json
 }
 
-async function requestSwapModel (model_title) {
+async function requestSwapModel(model_title) {
   console.log('requestSwapModel: ')
   // const full_url = 'http://127.0.0.1:8000/swapModel'
 
@@ -158,8 +158,33 @@ async function requestSwapModel (model_title) {
 
   return json
 }
+async function setInpaintMaskWeight(value) {
+  console.log('requestSwapModel: ')
+  // const full_url = 'http://127.0.0.1:8000/swapModel'
 
-async function requestInterrupt (model_title) {
+  const full_url = 'http://127.0.0.1:8000/sdapi/v1/options'
+  payload = {
+    inpainting_mask_weight: value
+  }
+  let request = await fetch(full_url, {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(payload)
+    // "body": payload
+  })
+
+  let json = await request.json()
+
+  console.log('models json:')
+  console.dir(json)
+
+  return json
+}
+
+async function requestInterrupt(model_title) {
   try {
     console.log('requestInterrupt: ')
     // const full_url = 'http://127.0.0.1:8000/swapModel'
@@ -191,62 +216,62 @@ async function requestInterrupt (model_title) {
   }
 }
 
-async function getVersionRequest(){
+async function getVersionRequest() {
   // version = "v0.0.0"
-    console.log('requestGetSamplers: ')
+  console.log('requestGetSamplers: ')
   try {
 
     const full_url = 'http://127.0.0.1:8000/version'
     let request = await fetch(full_url)
     let json = await request.json()
-    console.log('version json:',json)
+    console.log('version json:', json)
     version = json['version']
-    
+
     return version
-  }catch(e){
+  } catch (e) {
     console.warn(e)
     version = "v0.0.0"
 
     return version
   }
-  
+
 
 }
 
-async function changeSdUrl(new_sd_url){
+async function changeSdUrl(new_sd_url) {
   // version = "v0.0.0"
-    console.log('changeSdUrl: new_sd_url:', new_sd_url)
+  console.log('changeSdUrl: new_sd_url:', new_sd_url)
   try {
 
-    payload= {
+    payload = {
       "sd_url": new_sd_url
     }
 
     const full_url = 'http://127.0.0.1:8000/sd_url/'
     console.log("changeSdUrl: payload: ", payload)
-  let request = await fetch(full_url, {
-    method: 'POST',
-    headers: {
-      Accept: 'application/json',
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify(payload)
-    
-  })
+    let request = await fetch(full_url, {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(payload)
+
+    })
 
 
-    
+
     // let json = await request.json()
     // console.log('changeSdUrl:',json)
-    console.log('changeSdUrl: request: ',request)
-    
-    
-    
-  }catch(e){
+    console.log('changeSdUrl: request: ', request)
+
+
+
+  } catch (e) {
     console.warn(e)
-    
+
   }
-  
+
 
 }
 
@@ -256,12 +281,12 @@ async function changeSdUrl(new_sd_url){
 //   // var JSONInPrettyFormat = JSON.stringify(json, undefined, 4);
 //   // return 
 // }
-async function loadHistory (uniqueDocumentId) {
-  
+async function loadHistory(uniqueDocumentId) {
+
   let json = {}
   try {
     payload = {
-      "uniqueDocumentId":uniqueDocumentId
+      "uniqueDocumentId": uniqueDocumentId
     }
 
     const full_url = 'http://127.0.0.1:8000/history/load'
@@ -282,9 +307,9 @@ async function loadHistory (uniqueDocumentId) {
     console.warn(e)
   }
 
-  return [json['image_paths'],json['metadata_jsons']]
+  return [json['image_paths'], json['metadata_jsons']]
 }
-async function loadPromptShortcut () {
+async function loadPromptShortcut() {
   // console.log('loadPromptShortcut:')
   let json = {}
   try {
@@ -311,11 +336,11 @@ async function loadPromptShortcut () {
   return json['prompt_shortcut']
 }
 
-async function savePromptShortcut (prompt_shortcut) {
+async function savePromptShortcut(prompt_shortcut) {
   // console.log('loadPromptShortcut:')
   let json = {}
   try {
-    payload = {"prompt_shortcut":prompt_shortcut}
+    payload = { "prompt_shortcut": prompt_shortcut }
 
     const full_url = 'http://127.0.0.1:8000/prompt_shortcut/save'
 
@@ -351,5 +376,6 @@ module.exports = {
   changeSdUrl,
   loadPromptShortcut,
   savePromptShortcut,
-  loadHistory
+  loadHistory,
+  setInpaintMaskWeight,
 }
