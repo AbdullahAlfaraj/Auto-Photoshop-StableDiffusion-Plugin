99591f89a3b5167b259e3dce98abeda281f08f1f
Abdullah Alfaraj <abdullah.a.f2014@gmail.com>
2023-01-20 01:05:51 +0300

    request smaller number of images

--- server/python_server/search.py
+++ server/python_server/search.py
@@ -4,7 +4,7 @@ from duckduckgo_search import ddg_images
 async def imageSearch(keywords = 'cute cats'):
 
     r = ddg_images(keywords, region='wt-wt', safesearch='Off', size=None,
-                type_image=None, layout=None, license_image=None, max_results=300)
+                type_image=None, layout=None, license_image=None, max_results=30)
     
     # print(r)
     return r
