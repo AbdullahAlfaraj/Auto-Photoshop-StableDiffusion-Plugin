9e8a77388f95e6cb300fdeff8ae174999a6e0fa3
Abdullah Alfaraj <abdullah.a.f2014@gmail.com>
2023-02-03 14:15:47 +0300

    first attempt at making an auto1111 extension

---
+++ install.py
@@ -0,0 +1,1 @@
+print("hello world auto-photoshop-sd plugin")
---
+++ scripts/main.py
@@ -0,0 +1,11 @@
+from modules import scripts, processing, shared, images, devices, ui, lowvram
+import gradio
+import requests
+import time
+import PIL.Image
+import base64
+import io
+import os.path
+import numpy
+import itertools
+import torch
