9c307ed37af89bc92aaaa3bd442af632c1057900
Abdullah Alfaraj <abdullah.a.f2014@gmail.com>
2023-02-10 16:05:01 +0300

    another function for snap shot layer

--- utility/io.js
+++ utility/io.js
@@ -1,3 +1,113 @@
+const batchPlay = require('photoshop').action.batchPlay
+const psapi = require('../psapi')
+async function snapShotLayer() {
+    //snapshot layer with no mask
+    let command = [
+        // Select All Layers current layer
+        {
+            _obj: 'selectAllLayers',
+            _target: [
+                { _enum: 'ordinal', _ref: 'layer', _value: 'targetEnum' },
+            ],
+        },
+        // Duplicate current layer
+        // {"ID":[459,460,461,462,463,464,465,466,467,468,469,470,471,472,473,474,475,476,477,478,479,480,481,482,483,484,485,486,487,488,489,490,491,492,493,494,495,496,497,498,499,500,501,502,503,504,505,506,507,508,509,510,511,512,513],"_obj":"duplicate","_target":[{"_enum":"ordinal","_ref":"layer","_value":"targetEnum"}],"version":5},
+        {
+            // ID: ids,
+            _obj: 'duplicate',
+            _target: [
+                { _enum: 'ordinal', _ref: 'layer', _value: 'targetEnum' },
+            ],
+            // version: 5
+        },
+
+        // Merge Layers
+        { _obj: 'mergeLayersNew' },
+        // Make
+        {
+            _obj: 'make',
+            at: { _enum: 'channel', _ref: 'channel', _value: 'mask' },
+            new: { _class: 'channel' },
+            using: { _enum: 'userMaskEnabled', _value: 'revealSelection' },
+        },
+        // Set Selection
+        {
+            _obj: 'set',
+            _target: [{ _property: 'selection', _ref: 'channel' }],
+            to: { _enum: 'ordinal', _ref: 'channel', _value: 'targetEnum' },
+        },
+        //make a group
+        {
+            _obj: 'make',
+            _target: [
+                {
+                    _ref: 'layerSection',
+                },
+            ],
+            from: {
+                _ref: 'layer',
+                _enum: 'ordinal',
+                _value: 'targetEnum',
+            },
+            layerSectionStart: 512,
+            layerSectionEnd: 513,
+            name: 'Group 2',
+            _options: {
+                dialogOptions: 'dontDisplay',
+            },
+        },
+        {
+            _obj: 'mergeLayersNew',
+            _options: {
+                dialogOptions: 'dontDisplay',
+            },
+        },
+    ]
+
+    const result = await batchPlay(command, {
+        synchronousExecution: true,
+        modalBehavior: 'execute',
+    })
+
+    return result
+}
+
+async function snapShotLayerExe() {
+    await executeAsModal(async () => {
+        //create a fill layer above the background layer, so that it's present in the snapshot
+        try {
+            const selectionInfo = await psapi.getSelectionInfoExe()
+
+            // const backgroundLayer = await app.activeDocument.backgroundLayer
+
+            await psapi.createSolidLayer(255, 255, 255)
+            const solid_layer = await app.activeDocument.activeLayers[0]
+            // await psapi.unSelectMarqueeExe()//unselect the
+
+            // await solid_layer.moveAbove(backgroundLayer)
+            // await snapShotLayer() //create a layer with only the opaque pixels
+            // await psapi.reSelectMarqueeExe(selectionInfo)
+            // await solid_layer.delete()
+        } catch (e) {
+            console.warn(e)
+        }
+    })
+    await executeAsModal(async () => {
+        //create a fill layer above the background layer, so that it's present in the snapshot
+        try {
+            const solid_layer = await app.activeDocument.activeLayers[0]
+            const backgroundLayer = await app.activeDocument.backgroundLayer
+            await solid_layer.moveAbove(backgroundLayer)
+            await psapi.unselectActiveLayersExe()
+            await snapShotLayer() //create a layer with only the opaque pixels
+            // await psapi.reSelectMarqueeExe(selectionInfo)
+            // await psapi.unSelectMarqueeExe()//unselect the
+            await solid_layer.delete()
+        } catch (e) {
+            console.warn(e)
+        }
+    })
+}
 
 class IO {
     constructor() {
