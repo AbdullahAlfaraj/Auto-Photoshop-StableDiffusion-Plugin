0ae9d2896728566f217b685b5c3cc579cd1f5074
Abdullah Alfaraj <abdullah.a.f2014@gmail.com>
2023-02-16 14:06:38 +0300

    enum module to hold all Enum type structure

---
+++ enum.js
@@ -0,0 +1,10 @@
+const clickTypeEnum = {
+    Click: 'click',
+    ShiftClick: 'shift_click',
+    AltClick: 'alt_click',
+    SecondClick: 'second_click', //when we click a thumbnail that is active/ has orange border
+}
+
+module.exports = {
+    clickTypeEnum,
+}
