15e755726b739655dc28d5f0e9b537afa59b113b
Abdullah Alfaraj <abdullah.a.f2014@gmail.com>
2023-02-23 01:29:02 +0300

    add automatic status enum

--- enum.js
+++ enum.js
@@ -5,6 +5,14 @@ const clickTypeEnum = {
     SecondClick: 'second_click', //when we click a thumbnail that is active/ has orange border
 }
 
+const AutomaticStatusEnum = {
+    NoApi: 'no_api',
+    Offline: 'offline',
+    RunningNoApi: 'running_no_api',
+    RunningWithApi: 'running_with_api',
+}
+
 module.exports = {
     clickTypeEnum,
+    AutomaticStatusEnum,
 }
