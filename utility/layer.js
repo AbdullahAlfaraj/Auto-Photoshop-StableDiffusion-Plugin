3c643b379f789ab9e292ca4998587a04c21272c5
Abdullah Alfaraj <abdullah.a.f2014@gmail.com>
2023-02-25 04:19:18 +0300

    cover the background layer edge cases

--- utility/layer.js
+++ utility/layer.js
@@ -283,7 +283,15 @@ async function createBackgroundLayer(r = 255, g = 255, b = 255) {
             //no need to create a background layer
             return null
         }
+
+        //reselect the selection area if it exist
+
         await executeAsModal(async () => {
+            //store the selection area and then unselected
+            const selectionInfo = await psapi.getSelectionInfoExe()
+            await psapi.unSelectMarqueeExe()
+            const active_layers = app.activeDocument.activeLayers
+
             // await createNewLayerCommand('background') //create layer
             //make the layer into background
             const result = await batchPlay(
@@ -293,6 +301,9 @@ async function createBackgroundLayer(r = 255, g = 255, b = 255) {
                     modalBehavior: 'execute',
                 }
             )
+
+            await psapi.reSelectMarqueeExe(selectionInfo)
+            await psapi.selectLayersExe(active_layers)
         })
     } catch (e) {
         console.warn(e)
