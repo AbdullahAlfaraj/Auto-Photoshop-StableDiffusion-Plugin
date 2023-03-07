ea3438407dd40ebe3c38f0441e419539b7150293
evv <42819131+evvfebruary@users.noreply.github.com>
2023-03-07 19:33:39 +0700

    Specify python version

--- start_server.sh
+++ start_server.sh
@@ -6,12 +6,12 @@ export SD_URL=http://127.0.0.1:7860
 # Check if the desired environment exists
 if [ ! -d "server_env" ]; then
   # Create the environment if it doesn't exist
-  python -m venv server_env
+  python3 -m venv server_env
   source ./server_env/bin/activate
-  python -m pip install -r requirements.txt
+  python3 -m pip install -r requirements.txt
 else
   source ./server_env/bin/activate
 fi
 
 cd ./server/python_server
-python -m uvicorn serverMain:app --reload
+python3 -m uvicorn serverMain:app --reload
