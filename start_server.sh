aecb16c8f3e1bcb76a5116d0d34b48daffc2ed91
evv <42819131+evvfebruary@users.noreply.github.com>
2023-03-08 22:20:59 +0700

    Remove specific version

--- start_server.sh
+++ start_server.sh
@@ -23,12 +23,12 @@ fi
 # Check if the desired environment exists
 if [ ! -d "server_env" ]; then
   # Create the environment if it doesn't exist
-  python3 -m venv server_env
+  python -m venv server_env
   source ./server_env/bin/activate
-  python3 -m pip install -r requirements.txt
+  python -m pip install -r requirements.txt
 else
   source ./server_env/bin/activate
 fi
 
 cd ./server/python_server
-python3 -m uvicorn serverMain:app --reload
+python -m uvicorn serverMain:app --reload
