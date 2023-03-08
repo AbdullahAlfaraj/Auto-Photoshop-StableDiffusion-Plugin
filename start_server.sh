a8624941e9176bfd34bbcfbe1d0fd64aa1e6e262
evv <42819131+evvfebruary@users.noreply.github.com>
2023-03-08 22:20:02 +0700

    Add python version naive checker

--- start_server.sh
+++ start_server.sh
@@ -3,6 +3,23 @@ git pull
 # Set the desired remote host where the "<>" is
 export SD_URL=http://127.0.0.1:7860
 
+# Check python was installed
+if ! hash python; then
+    echo "Python is not installed"
+    exit 1
+fi
+
+# Check the default python version
+orig_ver=$(python -V 2>&1)
+ver=$( echo "$orig_ver" | sed 's/.* \([0-9]\).\([0-9]\).*/\1\2/')
+if [[ "$ver" -gt "37" ]] # Because of uvicorn==0.20.0 in requirements
+then
+    echo "You have valid version of ($orig_ver)"
+else
+    echo "Your version ($orig_ver) not valid, should be >3.7"
+    exit 1
+fi
+
 # Check if the desired environment exists
 if [ ! -d "server_env" ]; then
   # Create the environment if it doesn't exist
