ecb70d997edb2714a0c6bd4594a63c10e222342e
Abdullah Alfaraj <abdullah.a.f2014@gmail.com>
2023-03-11 18:08:45 +0300

    add controlnet hand fix preset

--- utility/presets/controlnet_preset.js
+++ utility/presets/controlnet_preset.js
@@ -27,9 +27,51 @@ const MaintainPositionSettings = {
     },
 }
 
+const Pose_Depth_Canny_HandFix = {
+    0: {
+        module: 'openpose',
+        model: 'control_sd15_openpose [fef5e48e]',
+        weight: 1,
+        resize_mode: null,
+        lowvram: null,
+        processor_res: null,
+        threshold_a: null,
+        threshold_b: null,
+        guidance_start: 0,
+        guidance_end: 1,
+        guessmode: null,
+    },
+    1: {
+        module: 'depth',
+        model: 'control_sd15_depth [fef5e48e]',
+        weight: 1.3,
+        resize_mode: null,
+        lowvram: null,
+        processor_res: null,
+        threshold_a: null,
+        threshold_b: null,
+        guidance_start: 0.3,
+        guidance_end: 1,
+        guessmode: null,
+    },
+    2: {
+        module: 'canny',
+        model: 'control_sd15_canny [fef5e48e]',
+        weight: 1.3,
+        resize_mode: null,
+        lowvram: null,
+        processor_res: null,
+        threshold_a: null,
+        threshold_b: null,
+        guidance_start: 0.3,
+        guidance_end: 1,
+        guessmode: null,
+    },
+}
+
 const ControlNetNativePresets = {
     'Maintain Position': MaintainPositionSettings,
-    'Maintain Position 2': MaintainPositionSettings,
+    'Hand Fix': Pose_Depth_Canny_HandFix,
 }
 module.exports = {
     ControlNetNativePresets,
