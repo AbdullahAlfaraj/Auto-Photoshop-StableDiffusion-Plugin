d30a884e0dbc8b08a97005e1cacb5c2e675acfe4
Abdullah Alfaraj <abdullah.a.f2014@gmail.com>
2023-05-13 19:31:54 +0300

    update duckduckgo_search to version 2.9.5

--- install.py
+++ install.py
@@ -40,6 +40,14 @@ if auto_update:
 
 # print("Auto-Photoshop-SD plugin is installing")
 
-if not launch.is_installed("duckduckgo_search"):
-    launch.run_pip("install duckduckgo_search==2.9.5", "requirements for Auto-Photoshop Image Search")
-    
+package_name = 'duckduckgo_search'
+package_version= '2.9.5'
+if not launch.is_installed(package_name):
+    launch.run_pip(f"install {package_name}=={package_version}", "requirements for Auto-Photoshop Image Search")
+else:# it's installed but we need to check for update
+    import pkg_resources
+
+    version = pkg_resources.get_distribution(package_name).version
+    if(version != package_version):
+        print(f'{package_name} version: {version} will update to version: {package_version}')
+        launch.run_pip(f"install {package_name}=={package_version}", "update requirements for Auto-Photoshop Image Search")
