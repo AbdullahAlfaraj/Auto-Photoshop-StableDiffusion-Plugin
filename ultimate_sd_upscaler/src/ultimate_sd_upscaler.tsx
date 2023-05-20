f11a9797c1cd9c6c841a740e7d489a6479b28e75
Abdullah Alfaraj <abdullah.a.f2014@gmail.com>
2023-05-20 04:15:16 +0300

    store the ultimate SD upscale forms data in mobx store

--- ultimate_sd_upscaler/src/ultimate_sd_upscaler.tsx
+++ ultimate_sd_upscaler/src/ultimate_sd_upscaler.tsx
@@ -1,12 +1,13 @@
-import React from 'react'
+import React, { ReactEventHandler } from 'react'
 import ReactDOM from 'react-dom/client'
 
-import { makeAutoObservable } from 'mobx'
-import { observer } from 'mobx-react'
+import { action, makeAutoObservable, reaction, toJS } from 'mobx'
+import { Provider, inject, observer } from 'mobx-react'
 
-import { SpMenu, SpSliderWithLabel } from './elements'
+import { SliderType, SpMenu, SpSliderWithLabel } from './elements'
 
 import * as sdapi from '../../sdapi_py_re'
+import { storeAnnotation } from 'mobx/dist/internal'
 
 let ui_config = {
     tile_width: {
@@ -69,6 +70,78 @@ let ui_config = {
         visible: false,
         interactive: true,
     },
+    redraw_mode: {
+        label: 'Type',
+        choices: ['Linear', 'Chess', 'None'],
+        type: 'index',
+        value: 0,
+    },
+    save_upscaled_image: {
+        label: 'Upscaled',
+        value: true,
+    },
+    save_seams_fix_image: {
+        label: 'Seams fix',
+        value: false,
+    },
+
+    seams_fix_type: {
+        label: 'Type',
+        choices: [
+            'None',
+            'Band pass',
+            'Half tile offset pass',
+            'Half tile offset pass + intersections',
+        ],
+        type: 'index',
+        value: 0,
+    },
+
+    target_size_type: {
+        label: 'Target size type',
+        choices: [
+            'From img2img2 settings',
+            'Custom size',
+            'Scale from image size',
+        ],
+        type: 'index',
+        value: 2,
+    },
+
+    custom_width: {
+        label: 'Custom width',
+        minimum: 64,
+        maximum: 8192,
+        step: 64,
+        value: 2048,
+        visible: false,
+        interactive: true,
+    },
+    custom_height: {
+        label: 'Custom height',
+        minimum: 64,
+        maximum: 8192,
+        step: 64,
+        value: 2048,
+        visible: false,
+        interactive: true,
+    },
+    custom_scale: {
+        label: 'Scale',
+        minimum: 1,
+        maximum: 16,
+        step: 0.01,
+        value: 2,
+        visible: false,
+        interactive: true,
+    },
+
+    upscaler_index: {
+        label: 'Upscaler',
+        choices: [],
+        type: 'index',
+        value: 0,
+    },
 }
 
 interface UltimateSDUpscalerData {
@@ -91,69 +164,99 @@ interface UltimateSDUpscalerData {
     custom_height: number
     custom_scale: number
 }
+export const script_args_ordered = [
+    '_',
+    'tile_width',
+    'tile_height',
+    'mask_blur',
+    'padding',
+    'seams_fix_width',
+    'seams_fix_denoise',
+    'seams_fix_padding',
+    'upscaler_index',
+    'save_upscaled_image',
+    'redraw_mode',
+    'save_seams_fix_image',
+    'seams_fix_mask_blur',
+    'seams_fix_type',
+    'target_size_type',
+    'custom_width',
+    'custom_height',
+    'custom_scale',
+]
 
 class UltimateSDUpscalerStore {
     data: UltimateSDUpscalerData
-
+    // test_value: number = 10
+    // test_value_2: number = 2
+    test_value: number
+    test_value_2: number
+    is_active: boolean
     constructor(data: UltimateSDUpscalerData) {
-        makeAutoObservable(this)
         this.data = data
+        this.test_value = 10
+        this.test_value_2 = 2
+        this.is_active = false
+        makeAutoObservable(this)
+
+        // reaction(
+        //     () => [this.test_value],
+        //     () => {
+        //         this.test_value_2 = this.test_value * 2
+        //         console.log('reaction to test_value change:', this.test_value)
+        //         console.log('this.test_value_2:', this.test_value_2)
+        //     }
+        // )
+    }
+    setIsActive(b_value: boolean) {
+        this.is_active = b_value
+    }
+    setTestValue(new_value: number) {
+        this.test_value = new_value
+        console.log('setTestValue: new_value ', new_value)
+        console.log('setTestValue: this.test_value: ', this.test_value)
     }
 
-    static create(data: UltimateSDUpscalerData) {
-        return new UltimateSDUpscalerStore(data)
+    updateProperty(key: keyof UltimateSDUpscalerData, value: any) {
+        ;(this.data as any)[key] = value
+    }
+
+    toJsFunc() {
+        return toJS(this)
+    }
+    orderedValues() {
+        const values = []
+        for (const key of script_args_ordered) {
+            // console.log(key, this.data[key])
+            values.push((this.data as any)[key])
+        }
+        return values
     }
 }
 
-const ultimate_sd_upscaler_store = UltimateSDUpscalerStore.create({
-    _: '',
-    tile_width: 512,
-    tile_height: 0,
-    mask_blur: 8,
-    padding: 32,
-    seams_fix_width: 64,
-    seams_fix_denoise: 0.275,
-    seams_fix_padding: 32,
-    upscaler_index: 3,
-    save_upscaled_image: false,
-    redraw_mode: 0,
-    save_seams_fix_image: true,
-    seams_fix_mask_blur: 8,
-    seams_fix_type: 3,
-    target_size_type: 2,
-    custom_width: 1080,
-    custom_height: 1440,
-    custom_scale: 1.875,
-})
-
-export let script_args: (string | number | boolean)[] = Object.values(
-    ultimate_sd_upscaler_store.data
+const configValues = Object.entries(ui_config).reduce(
+    (acc, [key, value]) => ({ ...acc, [key]: value.value }),
+    {}
 )
+const default_values: any = {
+    _: '',
+    ...configValues,
+}
+export const ultimate_sd_upscaler_store = new UltimateSDUpscalerStore(
+    default_values
+)
+
+// export let script_args: (string | number | boolean)[] = Object.values(
+//     ultimate_sd_upscaler_store.data
+// )
 export let script_name: string = 'Ultimate SD upscale'
 
-// class UltimateSDUpscalerStore {
-//     slider1Value = 0
-//     slider2Value = 0
-
-//     constructor() {
-//         makeAutoObservable(this)
-//     }
-
-//     setSlider1Value(newValue: any) {
-//         this.slider1Value = newValue
-//     }
-
-//     setSlider2Value(newValue: any) {
-//         this.slider2Value = newValue
-//     }
-// }
-
-// const scriptFormStore = new UltimateSDUpscalerStore()
-
 @observer
-class UltimateSDUpscalerForm extends React.Component {
-    slider1Ref = React.createRef<SpSliderWithLabel>()
-    slider2Ref = React.createRef<SpSliderWithLabel>()
+class UltimateSDUpscalerForm extends React.Component<{
+    store: UltimateSDUpscalerStore
+}> {
+    // slider1Ref = React.createRef<SpSliderWithLabel>()
+    // slider2Ref = React.createRef<SpSliderWithLabel>()
     state = {
         items: ['Item 1', 'Item 2', 'Item 3'],
         sd_upscalers: [],
@@ -167,21 +270,38 @@ class UltimateSDUpscalerForm extends React.Component {
         })
     }
     handleSlider1ValueChange = (newValue: any) => {
-        // scriptFormStore.setSlider1Value(newValue)
+        // this.props.store.setTestValue(newValue)
+        this.props.store.test_value = newValue
+
+        // this.props.store.
+        console.log('store.test_value: ', this.props.store.test_value)
+        console.log('newValue: ', newValue)
     }
 
     handleSlider2ValueChange = (newValue: any) => {
         // scriptFormStore.setSlider2Value(newValue)
     }
-
+    handleSliderChange = (key: any, newValue: any) => {
+        this.props.store.updateProperty(key, newValue)
+    }
+    handleMenuChange = (key: any, new_index_value_pair: any) => {
+        let config = ui_config[key as keyof typeof ui_config] as any
+        if ('type' in config) {
+            let value =
+                config.type === 'index'
+                    ? new_index_value_pair['index']
+                    : new_index_value_pair['item']
+            this.props.store.updateProperty(key, value)
+        }
+    }
     autoFillSliders = (newValue: any) => {
         // scriptFormStore.setSlider1Value(newValue)
-        if (this.slider1Ref.current) {
-            this.slider1Ref.current.setSliderValue(newValue)
-        }
-        if (this.slider2Ref.current) {
-            this.slider2Ref.current.setSliderValue(newValue)
-        }
+        // if (this.slider1Ref.current) {
+        //     this.slider1Ref.current.setSliderValue(newValue)
+        // }
+        // if (this.slider2Ref.current) {
+        //     this.slider2Ref.current.setSliderValue(newValue)
+        // }
         // scriptFormStore.setSlider2Value(newValue)
     }
 
@@ -195,56 +315,177 @@ class UltimateSDUpscalerForm extends React.Component {
     }
 
     render() {
+        const ids = [
+            'tile_width',
+            'tile_height',
+            'mask_blur',
+            'padding',
+        ] as const
+        // let config = ui_config[ids as keyof typeof ui_config] as any
+        const group_1_sliders = ids.map((id) => (
+            <SpSliderWithLabel
+                key={id}
+                id={id}
+                show-value={false}
+                steps={ui_config[id].step}
+                out_min={ui_config[id].minimum}
+                out_max={ui_config[id].maximum}
+                output_value={ui_config[id].value}
+                title="this is a title"
+                label={ui_config[id].label}
+                onSliderChange={this.handleSliderChange}
+            />
+        ))
+        const seamfix_ids = [
+            'seams_fix_denoise',
+            'seams_fix_width',
+            'seams_fix_mask_blur',
+            'seams_fix_padding',
+        ] as const
+        const seamfix_sliders = seamfix_ids.map((id) => (
+            <SpSliderWithLabel
+                key={id}
+                id={id}
+                show-value={false}
+                steps={ui_config[id].step}
+                out_min={ui_config[id].minimum}
+                out_max={ui_config[id].maximum}
+                output_value={ui_config[id].value}
+                title="this is a title"
+                label={ui_config[id].label}
+                onSliderChange={this.handleSliderChange}
+                slider_type={
+                    Number.isInteger(ui_config[id].step)
+                        ? SliderType.Integer
+                        : SliderType.Float
+                }
+            />
+        ))
         return (
             <div>
-                <SpSliderWithLabel
-                    ref={this.slider1Ref}
-                    // onSliderValueChangeCallback={this.handleSlider1ValueChange}
-                    id="slider_1"
-                    show-value={false}
-                    min={0}
-                    max={100}
-                    value={30}
-                    title="this is a title"
-                    label={ui_config.tile_width.label}
-                />
-                <SpSliderWithLabel
-                    ref={this.slider2Ref}
-                    label={ui_config.tile_height.label}
-                    // onSliderValueChangeCallback={this.handleSlider2ValueChange}
-                />
-                <SpSliderWithLabel
-                    ref={this.slider2Ref}
-                    label={ui_config.mask_blur.label}
-                    // onSliderValueChangeCallback={this.handleSlider2ValueChange}
-                />
-                <SpSliderWithLabel
-                    ref={this.slider2Ref}
-                    label={ui_config.padding.label}
-                    // onSliderValueChangeCallback={this.handleSlider2ValueChange}
-                />
-                <SpSliderWithLabel
-                    ref={this.slider2Ref}
-                    label={ui_config.seams_fix_denoise.label}
-                />
-                <SpSliderWithLabel
-                    ref={this.slider2Ref}
-                    label={ui_config.seams_fix_mask_blur.label}
-                />
-                <SpSliderWithLabel
-                    ref={this.slider2Ref}
-                    label={ui_config.seams_fix_padding.label}
-                />
-                <SpSliderWithLabel
-                    ref={this.slider2Ref}
-                    label={ui_config.seams_fix_denoise.label}
-                />
+                <sp-checkbox
+                    checked={this.props.store.is_active ? true : undefined}
+                    onClick={(event: React.ChangeEvent<HTMLInputElement>) => {
+                        this.props.store.setIsActive(event.target.checked)
+                    }}
+                >
+                    {'Activate'}
+                </sp-checkbox>
+                <button
+                    onClick={(event) =>
+                        this.handleSlider1ValueChange(
+                            this.props.store.test_value + 1
+                        )
+                    }
+                ></button>
                 <SpMenu
                     title="Stable Diffusion Upscalers"
                     items={this.state.sd_upscalers}
                     // style="width: 199px; margin-right: 5px"
+                    label_item="Select Upscaler"
+                    id={'upscaler_index'}
+                    onChange={this.handleMenuChange}
+                />
+                {/* <SpSliderWithLabel
+                    // ref={this.slider1Ref}
+                    // onSliderValueChangeCallback={this.handleSlider1ValueChange}
+                    id="tile_width"
+                    show-value={false}
+                    steps={64}
+                    out_min={ui_config.tile_width.minimum}
+                    out_max={ui_config.tile_width.maximum}
+                    // slider_value={this.props.store.test_value}
+                    output_value={parseInt(ui_config.tile_width.label)}
+                    title="this is a title"
+                    label={ui_config.tile_width.label}
+                    onSliderChange={this.handleSliderChange}
                 />
 
+                <p>test_value: {this.props.store.test_value}</p>
+                <SpSliderWithLabel
+                    // ref={this.slider2Ref}
+                    label={ui_config.tile_height.label}
+                    // onSliderValueChangeCallback={this.handleSlider2ValueChange}
+                />
+                 */}
+                {group_1_sliders}
+                {seamfix_sliders}
+                {/* <SpSliderWithLabel
+                    label={ui_config.mask_blur.label}
+                    // onSliderValueChangeCallback={this.handleSlider2ValueChange}
+                />
+                <SpSliderWithLabel
+                    label={ui_config.padding.label}
+                    // onSliderValueChangeCallback={this.handleSlider2ValueChange}
+                /> */}
+
+                <SpMenu
+                    title=""
+                    id={'target_size_type'}
+                    items={ui_config.target_size_type.choices}
+                    label_item={'Select ' + ui_config.target_size_type.label}
+                    onChange={this.handleMenuChange}
+                    // style="width: 199px; margin-right: 5px"
+                />
+                <SpSliderWithLabel
+                    label={ui_config.custom_scale.label}
+                    output_value={this.props.store.data.custom_scale}
+                    id={'custom_scale'}
+                    out_min={ui_config.custom_scale.minimum}
+                    out_max={ui_config.custom_scale.maximum}
+                    onSliderChange={this.handleSliderChange}
+                    steps={0.01}
+                    slider_type={SliderType.Float}
+                />
+
+                <SpMenu
+                    title="Seams Fix Type"
+                    id={'seams_fix_type'}
+                    items={ui_config.seams_fix_type.choices}
+                    label_item="Select Seams Fix Type"
+                    onChange={this.handleMenuChange}
+                    // style="width: 199px; margin-right: 5px"
+                />
+                {/* <SpSliderWithLabel label={ui_config.seams_fix_denoise.label} /> */}
+                {/* <SpSliderWithLabel label={ui_config.seams_fix_width.label} />
+                <SpSliderWithLabel
+                    label={ui_config.seams_fix_mask_blur.label}
+                    slider_value={this.props.store.data.seams_fix_mask_blur}
+                    id={'seams_fix_mask_blur'}
+                    onSliderChange={this.handleSliderChange}
+                />
+                <SpSliderWithLabel label={ui_config.seams_fix_padding.label} /> */}
+
+                <sp-checkbox
+                    checked={
+                        this.props.store.data.save_upscaled_image
+                            ? true
+                            : undefined
+                    }
+                    onClick={(event: React.ChangeEvent<HTMLInputElement>) => {
+                        this.props.store.updateProperty(
+                            'save_upscaled_image',
+                            event.target.checked
+                        )
+                    }}
+                >
+                    {ui_config.save_upscaled_image.label}
+                </sp-checkbox>
+                <sp-checkbox
+                    checked={
+                        this.props.store.data.save_seams_fix_image
+                            ? true
+                            : undefined
+                    }
+                    onClick={(event: React.ChangeEvent<HTMLInputElement>) => {
+                        this.props.store.updateProperty(
+                            'save_seams_fix_image',
+                            event.target.checked
+                        )
+                    }}
+                >
+                    {ui_config.save_seams_fix_image.label}
+                </sp-checkbox>
                 <button onClick={() => this.autoFillSliders(50)}>
                     Auto-fill sliders
                 </button>
@@ -256,39 +497,32 @@ class UltimateSDUpscalerForm extends React.Component {
     }
 }
 
-@observer
-class SliderValuesDisplay extends React.Component {
-    render() {
-        return (
-            <div>
-                <p>
-                    Slider 1 value: {ultimate_sd_upscaler_store.data.tile_width}
-                </p>
-                <p>
-                    Slider 2 value:{' '}
-                    {ultimate_sd_upscaler_store.data.tile_height}
-                </p>
-            </div>
-        )
-    }
-}
-
-function MyComponent() {
-    return (
-        <div>
-            <UltimateSDUpscalerForm />
-            <SliderValuesDisplay />
-            {/* ... */}
-        </div>
-    )
-}
+// @observer
+// class SliderValuesDisplay extends React.Component {
+//     render() {
+//         return (
+//             <div>
+//                 <p>
+//                     Slider test_value: {ultimate_sd_upscaler_store.test_value}
+//                 </p>
+//                 <p>
+//                     {/* Slider 1 value: {ultimate_sd_upscaler_store.data.tile_width} */}
+//                 </p>
+//                 <p>
+//                     Slider 2 value:{' '}
+//                     {ultimate_sd_upscaler_store.data.tile_height}
+//                 </p>
+//             </div>
+//         )
+//     }
+// }
 
 const domNode = document.getElementById('ultimateSDUpscalerContainer')!
 const root = ReactDOM.createRoot(domNode)
 root.render(
     <React.StrictMode>
-        {/* <SpSliderWithLabel onSliderValueChange={handleSliderValueChange} /> */}
-        <UltimateSDUpscalerForm />
-        <SliderValuesDisplay />
+        <UltimateSDUpscalerForm store={ultimate_sd_upscaler_store} />
+
+        {/* <SliderValuesDisplay /> */}
     </React.StrictMode>
 )
