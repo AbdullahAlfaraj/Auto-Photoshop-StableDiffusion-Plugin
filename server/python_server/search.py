a7c288e06d2312f8adfb7b5f2964955a5e861e1e
Abdullah Alfaraj <abdullah.a.f2014@gmail.com>
2023-05-28 00:48:19 +0300

    update image search to use the new duckduckgo_search 3.6.0 api

--- server/python_server/search.py
+++ server/python_server/search.py
@@ -1,6 +1,8 @@
 
 try:
-    from duckduckgo_search import ddg_images
+    # from duckduckgo_search import ddg_images
+    from duckduckgo_search import DDGS
+
 except ImportError:
     raise ImportError(
         "duckduckgo_search is required to image search. Please install it with `pip install duckduckgo_search`."
@@ -8,19 +10,46 @@ except ImportError:
 
 
 
+# async def imageSearch(keywords = 'cute cats'):
+
+#     r = ddg_images(keywords, region='wt-wt', safesearch='Off', size=None,
+#                 type_image=None, layout=None, license_image=None, max_results=30)
+    
+#     # print(r)
+#     return r
+
+
+
 async def imageSearch(keywords = 'cute cats'):
 
-    r = ddg_images(keywords, region='wt-wt', safesearch='Off', size=None,
-                type_image=None, layout=None, license_image=None, max_results=30)
-    
-    # print(r)
-    return r
+    from duckduckgo_search import DDGS
+
+    ddgs = DDGS(timeout=60)
+
+    keywords = 'butterfly'
+    ddgs_images_gen = ddgs.images(
+        keywords,
+        region="wt-wt",
+        safesearch="Off",
+        size=None,
+        color="Monochrome",
+        type_image=None,
+        layout=None,
+        license_image=None,
+    )
+    # for r in ddgs_images_gen:
+    #     print(r)
+    return list(ddgs_images_gen)
+
 
 if __name__ == "__main__":
+
+
     async def main():
         result = await imageSearch()
-        print(result)
-
+        print("result: ",result)
+        # result = await imageSearch2()
+        # print(result)
     import asyncio
     asyncio.run(main())
     
