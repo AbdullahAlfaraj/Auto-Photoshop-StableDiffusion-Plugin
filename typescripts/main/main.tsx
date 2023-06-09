256ccc83b9cd7c0afa2169360b116e55cffe9fcd
Abdullah Alfaraj <abdullah.a.f2014@gmail.com>
2023-06-09 07:02:37 +0300

    refix: select the current vae when retrieving vae list

--- typescripts/main/main.tsx
+++ typescripts/main/main.tsx
@@ -6,6 +6,8 @@ import { SpMenu } from '../util/elements'
 
 import { api, python_replacement } from '../util/oldSystem'
 const { getExtensionUrl } = python_replacement
+
+declare let g_sd_url: string
 // class SDStore extends AStore {
 //     constructor(data: any) {
 //         super(data)
@@ -23,9 +25,10 @@ const { getExtensionUrl } = python_replacement
 
 const default_values: any = {
     vae_model_list: [],
+    current_vae: '',
 }
 
-export const sd_store = new AStore(default_values)
+export const store = new AStore(default_values)
 
 @observer
 export class VAEComponent extends React.Component<{
@@ -40,41 +43,67 @@ export class VAEComponent extends React.Component<{
             console.warn('changeVAEModel: vae_model: ', vae_model, e)
         }
     }
+    handleRefresh() {
+        populateVAE()
+    }
     render(): React.ReactNode {
         return (
-            <SpMenu
-                title="vae models"
-                items={sd_store.data.vae_model_list}
-                // disabled={script_store.disabled}
-                // style="width: 199px; margin-right: 5px"
-                label_item="Select A VAE"
-                // id={'model_list'}
-                onChange={(id: any, value: any) => {
-                    // script_store.setSelectedScript(value.item)
-                    console.log('onChange value: ', value)
-                    this.changeVAEModel(value.item)
-                }}
-            ></SpMenu>
+            <div style={{ display: 'flex' }}>
+                <SpMenu
+                    title="vae models"
+                    items={store.data.vae_model_list}
+                    // disabled={script_store.disabled}
+                    // style="width: 199px; margin-right: 5px"
+                    label_item="Select A VAE"
+                    // id={'model_list'}
+                    selected_index={store.data.vae_model_list.indexOf(
+                        store.data.current_vae
+                    )}
+                    onChange={(id: any, value: any) => {
+                        // script_store.setSelectedScript(value.item)
+                        console.log('onChange value: ', value)
+                        this.changeVAEModel(value.item)
+                    }}
+                ></SpMenu>
+                <button
+                    className="btnSquare refreshButton"
+                    title="Refresh VAE Models List"
+                    onClick={this.handleRefresh}
+                ></button>
+            </div>
         )
     }
 }
 const vaeContainerNode = document.getElementById('settingsVAEContainer')!
 const vaeRoot = ReactDOM.createRoot(vaeContainerNode)
 
-let vae_model_list = ['None']
-declare let g_sd_url: string
 
-async function populateVAE() {
-    const extension_url = getExtensionUrl()
+async function requestGetVAE() {
+    const full_url = `${g_sd_url}/sdapi/v1/options`
 
-    const full_url = `${extension_url}/vae/list`
-
-    const vae_models = await api.requestGet(full_url)
-    console.log('populateVAE vae_models: ', vae_models)
-    // vae_model_list = vae_models
-    sd_store.updateProperty('vae_model_list', vae_models)
+    const options = await api.requestGet(full_url)
+    return options?.sd_vae
 }
-populateVAE()
+export async function populateVAE() {
+    try{
+
+        const extension_url = getExtensionUrl()
+        
+    const full_url = `${extension_url}/vae/list`
+    const vae_models = await api.requestGet(full_url)
+
+    console.log('populateVAE vae_models: ', vae_models)
+    store.updateProperty('vae_model_list', vae_models)
+
+    const current_vae = await requestGetVAE()
+    if (current_vae && vae_models.includes(current_vae)) {
+        store.updateProperty('current_vae', current_vae)
+    }
+}catch(e){
+    console.warn("populateVAE():", e)
+}
+}
+
 
 vaeRoot.render(
     <React.StrictMode>
