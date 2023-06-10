0ce2f0d5f331ac7bbfdd38f95fcce0f5bcd7b7f0
unknown <348063288@qq.com>
2023-06-10 14:28:37 +0800

    Chinese localization for controlnet

---
+++ typescripts/globalstore.ts
@@ -0,0 +1,12 @@
+import { observable } from "mobx";
+import {host} from 'uxp'
+
+interface GlobalStore {
+    Locale: 'zh_CN' | 'default',
+}
+
+var globalStore = observable<GlobalStore>({
+    Locale: host.uiLocale == 'zh_CN' ? host.uiLocale : 'default' 
+});
+
+export default globalStore;
