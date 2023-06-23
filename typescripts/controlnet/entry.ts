8ef37aea724975cc1cf30ca9d296bdcd447bb918
Abdullah Alfaraj <abdullah.a.f2014@gmail.com>
2023-06-23 10:53:53 +0300

    fix issue with stylization and typescript compiler directive

--- typescripts/controlnet/entry.ts
+++ typescripts/controlnet/entry.ts
@@ -124,9 +124,9 @@ function setControlMaskSrc(base64: string, index: number) {
     store.controlNetUnitData[index].mask = base64
 }
 function isControlNetModeEnable() {
-    //@ts-ignore
     let is_tab_enabled =
         !document.getElementById('chDisableControlNetTab') ||
+        //@ts-ignore
         !document.getElementById('chDisableControlNetTab').checked
 
     let numOfEnabled = 0
