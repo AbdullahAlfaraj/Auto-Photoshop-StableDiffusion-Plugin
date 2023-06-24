a3a813f7c47f6b61261cc27c49fbf3af106e0929
unknown <348063288@qq.com>
2023-06-24 22:26:20 +0800

    prettier

--- typescripts/globalstore.ts
+++ typescripts/globalstore.ts
@@ -1,12 +1,12 @@
-import { observable } from "mobx";
+import { observable } from 'mobx'
 import { host } from 'uxp'
 
 interface GlobalStore {
-    Locale: 'zh_CN' | 'default',
+    Locale: 'zh_CN' | 'default'
 }
 
 var globalStore = observable<GlobalStore>({
-    Locale: host.uiLocale == 'zh_CN' ? host.uiLocale : 'default' 
-});
+    Locale: host.uiLocale == 'zh_CN' ? host.uiLocale : 'default',
+})
 
-export default globalStore;
+export default globalStore
--- typescripts/locale/locale.ts
+++ typescripts/locale/locale.ts
@@ -1,18 +1,18 @@
-import globalStore from "../globalstore"
-import zhHans from './zh_CN/zh_Hans.json';
-import zhHansForPSPlugin from "./zh_CN/zh_Hans_PS.json"
+import globalStore from '../globalstore'
+import zhHans from './zh_CN/zh_Hans.json'
+import zhHansForPSPlugin from './zh_CN/zh_Hans_PS.json'
 
-export default function Locale(key: keyof typeof zhHans | keyof typeof zhHansForPSPlugin): string
-{
-    const locale = globalStore.Locale;
-    let res = '';
-    if (locale == 'zh_CN')
-    {
+export default function Locale(
+    key: keyof typeof zhHans | keyof typeof zhHansForPSPlugin
+): string {
+    const locale = globalStore.Locale
+    let res = ''
+    if (locale == 'zh_CN') {
         //@ts-ignore
-        if (key in zhHansForPSPlugin) res = zhHansForPSPlugin[key];
+        if (key in zhHansForPSPlugin) res = zhHansForPSPlugin[key]
         //@ts-ignore
-        if (key in zhHans) res = zhHans[key];
+        if (key in zhHans) res = zhHans[key]
     }
     res = res || key
-    return res;
+    return res
 }
