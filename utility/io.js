5f158f84b6c7623a99358db480dfb19a51fe4801
Abdullah Alfaraj <abdullah.a.f2014@gmail.com>
2023-07-10 11:04:44 +0300

    keep black edges instead of black corners, fro more stability.

--- utility/io.js
+++ utility/io.js
@@ -1314,15 +1314,15 @@ async function convertBlackToTransparentKeepBorders(base64) {
             width,
             height,
             function (x, y, idx) {
-                // if (x === 0 || y === 0 || x === width - 1 || y === height - 1)
-                // return
-                if (
-                    (x === 0 && y === 0) ||
-                    (x === 0 && y === height - 1) ||
-                    (x === width - 1 && y === 0) ||
-                    (x === width - 1 && y === height - 1)
-                )
+                if (x === 0 || y === 0 || x === width - 1 || y === height - 1)
                     return
+                // if (
+                //     (x === 0 && y === 0) ||
+                //     (x === 0 && y === height - 1) ||
+                //     (x === width - 1 && y === 0) ||
+                //     (x === width - 1 && y === height - 1)
+                // )
+                //     return
                 const red = this.bitmap.data[idx + 0]
                 const green = this.bitmap.data[idx + 1]
                 const blue = this.bitmap.data[idx + 2]
