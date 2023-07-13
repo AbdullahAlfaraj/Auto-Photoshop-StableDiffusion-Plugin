e398f7ac05507defa3f62bb1a39d4f221eceffbc
Abdullah Alfaraj <abdullah.a.f2014@gmail.com>
2023-07-13 16:02:35 +0300

    comment mobx enforced action

--- typescripts/entry.ts
+++ typescripts/entry.ts
@@ -1,7 +1,7 @@
-import { configure } from 'mobx'
-configure({
-    enforceActions: 'never', // disable mobx warning temporarily
-})
+// import { configure } from 'mobx'
+// configure({
+//     enforceActions: 'never', // disable mobx warning temporarily
+// })
 export * as control_net from './controlnet/entry'
 export * as after_detailer_script from './after_detailer/after_detailer'
 export * as ultimate_sd_upscaler from './ultimate_sd_upscaler/ultimate_sd_upscaler'
