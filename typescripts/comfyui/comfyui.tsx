fc4b125db1e2eabc37661de64a9dadb82f579e23
Abdullah Alfaraj <abdullah.a.f2014@gmail.com>
2023-09-15 01:03:44 +0300

    get valid_nodes definitions from comfy_api config node

--- typescripts/comfyui/comfyui.tsx
+++ typescripts/comfyui/comfyui.tsx
@@ -256,6 +256,10 @@ export async function getConfig() {
             history_result?.[prompt_id].outputs
         )[0]
         store.data.comfyui_config = { ...config }
+        store.data.comfyui_config.valid_nodes = JSON.parse(
+            config.valid_nodes.join('')
+        )
+
         return config
     } catch (e) {
         console.error(e)
@@ -292,75 +296,6 @@ enum InputTypeEnum {
     Menu = 'Menu',
     ImageBase64 = 'ImageBase64',
 }
-const ValidNodes: any = {
-    KSampler: {
-        inputs: {
-            seed: 'TextField',
-            steps: 'NumberField',
-            cfg: 'NumberField',
-            sampler_name: 'Menu',
-            scheduler: 'Menu',
-            denoise: 'NumberField',
-        },
-        list_id: {
-            sampler_name: 'samplers',
-            scheduler: 'schedulers',
-        },
-    },
-    EmptyLatentImage: {
-        inputs: {
-            width: 'NumberField',
-            height: 'NumberField',
-            batch_size: 'NumberField',
-        },
-    },
-    CLIPTextEncode: {
-        inputs: {
-            text: 'TextArea',
-        },
-    },
-
-    LatentUpscale: {
-        inputs: {
-            upscale_method: 'Menu',
-            width: 'NumberField',
-            height: 'NumberField',
-            crop: 'Menu',
-        },
-        list_id: {
-            upscale_method: 'latent_upscale_methods',
-            crop: 'latent_upscale_crop_methods',
-        },
-    },
-    CheckpointLoaderSimple: {
-        inputs: {
-            ckpt_name: 'Menu',
-        },
-        list_id: {
-            ckpt_name: 'checkpoints',
-        },
-    },
-    LoadImage: {
-        inputs: {
-            image: 'ImageBase64',
-        },
-    },
-    LoadImageBase64: {
-        inputs: {
-            image_base64: 'ImageBase64',
-        },
-    },
-    LoraLoader: {
-        inputs: {
-            lora_name: 'Menu',
-            strength_model: 'NumberField',
-            strength_clip: 'NumberField',
-        },
-        list_id: {
-            lora_name: 'loras',
-        },
-    },
-}
 
 interface ComfyUINode {
     inputs: any
@@ -374,6 +309,7 @@ function filterObjectProperties(node_inputs: any, valid_keys: string[]) {
 export function parseUIFromNode(node: ComfyUINode, node_id: string) {
     //convert node to array of ui element definition
     try {
+        const ValidNodes = store.data.comfyui_config?.valid_nodes
         const valid_ui = ValidNodes[node.class_type]?.inputs // all the valid inputs of a node
         const list_ids = ValidNodes[node.class_type]?.list_id
         if (valid_ui) {
